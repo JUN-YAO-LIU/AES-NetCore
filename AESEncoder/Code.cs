db7654f5a100dea3a28522b15d2713827fc1d87c
jim.liu <jim.liu@numiner.io>
2022-10-21 11:38:56 +0800

    first commit

---
+++ AESEncoder/Code.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://dotblogs.com.tw/shadow/2019/03/20/232821
+
+namespace AESEncoder
+{
+    public class Code
+    {
+        public class CodeConfiguration
+        {
+            public string Key { get; set; }
+            public string IV { get; set; }
+            public string Pwd { get; set; }
+        }
+
+        public CodeConfiguration Encoder(string pwd)
+        {
+            var sourceBytes = Encoding.UTF8.GetBytes(pwd);
+
+            var aes = Aes.Create();
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            // aes key 有支援的格式長度，所以字串轉不出這種長度就會出錯
+            //aes.Key = Encoding.UTF8.GetBytes(_key);
+
+            // aes iv 有支援的格式長度 長度要是 key的一半
+            //aes.IV = Encoding.UTF8.GetBytes("123");
+
+            aes.Key = GenerateBitsOfRandomEntropy(32);
+            aes.IV = GenerateBitsOfRandomEntropy(16);
+
+            var transform = aes.CreateEncryptor();
+
+            return new CodeConfiguration()
+            {
+                Key = Convert.ToBase64String(aes.Key),
+                IV = Convert.ToBase64String(aes.IV),
+                Pwd = Convert.ToBase64String(transform.TransformFinalBlock(sourceBytes, 0, sourceBytes.Length))
+            };
+        }
+
+        private static byte[] GenerateBitsOfRandomEntropy(int num)
+        {
+            // 32 Bytes will give us 256 bits.
+            var randomBytes = new byte[num];
+
+            Random genByteValue = new Random();
+            genByteValue.NextBytes(randomBytes);
+
+            return randomBytes;
+        }
+    }
+}
---
+++ AESEncoder/Program.cs
@@ -0,0 +1,13 @@
+﻿using AESEncoder;
+
+Console.WriteLine("輸入密碼:");
+
+var p1 = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(p1))
+{
+    var c = new Code();
+    var result = c.Encoder(p1);
+
+    Console.WriteLine("Key：" + result.Key + "\n" + "IV：" + result.IV + "\n" + "密碼：" + result.Pwd);
+}
